e86883184f4083f7a5c8bb3df24c9035526b25ec
Gonçalo Ferreira <gonmf@sapo.pt>
2018-05-06 21:15:42 +0100

    Cleanup some old files

--- app/javascript/phantasma.js
+++ app/javascript/phantasma.js
@@ -1,95 +1,45 @@
 var $ = require("jquery");
 neonJs = require('@cityofzion/neon-js');
 
-// Update this data from Phantasma
-var dummyInboxInitialData = [
-  {
-    date: Date.now(),
-    message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...',
-    sender: Math.random().toString(16).substring(2)
-  },
-  {
-    date: Date.now(),
-    message: 'Etiam eget ligula eu lectus lobortis condimentum. Aliquam ...',
-    sender: Math.random().toString(16).substring(2)
-  },
-  {
-    date: Date.now(),
-    message: 'Pellentesque habitant morbi tristique senectus et netus et ...',
-    sender: Math.random().toString(16).substring(2)
-  },
-  {
-    date: Date.now(),
-    message: 'Nulla at risus. Quisque purus magna, auctor et, sagittis ...',
-    sender: Math.random().toString(16).substring(2)
-  }
-];
-
-var dummyWallets = [
-  {
-    name: 'demo1@phantasma.io',
-    address: Math.random().toString(16).substring(2),
-    privateKey: Math.random().toString(16).substring(2)
-  },
-  {
-    name: 'demo2@phantasma.io',
-    address: Math.random().toString(16).substring(2),
-    privateKey: Math.random().toString(16).substring(2)
-  }
-];
-
-PH_ITEMS_PER_PAGE = 50;
-
-window.timestampToDateString = function(timestamp) {
-  return new Date(timestamp).toISOString().replace('T', ' ').substring(0, 19);
-}
+var demoSender = {
+                   name: 'Saturn Moshi',
+                   address: Math.random().toString(16).substring(2)
+                 };
+var demoWallet = {
+                   name: 'my-new-demo@phantasma.io',
+                   address: Math.random().toString(16).substring(2),
+                   privateKey: Math.random().toString(16).substring(2)
+                 };
 
 window.PH = {
   state: {
     mainWallet: 0,
-    wallets: [
-      {
-        name: 'my-new-demo@phantasma.io',
-        address: Math.random().toString(16).substring(2),
-        privateKey: Math.random().toString(16).substring(2)
-      }
-    ],
+    wallets: [demoWallet],
     section: 'inbox',
     inbox: {
       items: [
         {
-          title: 'Phantasma Project',
+          title: 'Abstract',
           time: 1525623012,
           body: "A purely peer-to-peer version of electronic cash would allow online payments to be sent directly from one party to another without going through a financial institution. Digital signatures provide part of the solution, but the main benefits are lost if a trusted third party is still required to prevent double-spending. We propose a solution to the double-spending problem using a peer-to-peer network. The network timestamps transactions by hashing them into an ongoing chain of hash-based proof-of-work, forming a record that cannot be changed without redoing the proof-of-work. The longest chain not only serves as proof of the sequence of events witnessed, but proof that it came from the largest pool of CPU power. As long as a majority of CPU power is controlled by nodes that are not cooperating to attack the network, they'll generate the longest chain and outpace attackers.<br/><br/>Cheers,<br/>PH Dev",
-          from: Math.random().toString(16).substring(2),
-          from_name: 'Mark Marcus',
-          to: Math.random().toString(16).substring(2)
+          from: demoSender.address,
+          from_name: demoSender.name,
+          to: demoWallet.address
         },
         {
-          title: 'Phantasma Project #2',
+          title: 'Introduction',
           time: 1525623012,
-          body: "A purely peer-to-peer version of electronic cash would allow online payments to be sent directly from one party to another without going through a financial institution. Digital signatures provide part of the solution, but the main benefits are lost if a trusted third party is still required to prevent double-spending. We propose a solution to the double-spending problem using a peer-to-peer network. The network timestamps transactions by hashing them into an ongoing chain of hash-based proof-of-work, forming a record that cannot be changed without redoing the proof-of-work. The longest chain not only serves as proof of the sequence of events witnessed, but proof that it came from the largest pool of CPU power. As long as a majority of CPU power is controlled by nodes that are not cooperating to attack the network, they'll generate the longest chain and outpace attackers.<br/><br/>Cheers,<br/>PH Dev",
-          from: Math.random().toString(16).substring(2),
-          from_name: 'Ruby Rubicon',
-          to: Math.random().toString(16).substring(2)
+          body: "Commerce on the Internet has come to rely almost exclusively on financial institutions serving as trusted third parties to process electronic payments. While the system works well enough for most transactions, it still suffers from the inherent weaknesses of the trust based model. Completely non-reversible transactions are not really possible, since financial institutions cannot avoid mediating disputes. The cost of mediation increases transaction costs, limiting the minimum practical transaction size and cutting off the possibility for small casual transactions, and there is a broader cost in the loss of ability to make non-reversible payments for nonreversible services. With the possibility of reversal, the need for trust spreads. Merchants must be wary of their customers, hassling them for more information than they would otherwise need. A certain percentage of fraud is accepted as unavoidable. These costs and payment uncertainties can be avoided in person by using physical currency, but no mechanism exists to make payments over a communications channel without a trusted party.",
+          from: demoSender.address,
+          from_name: demoSender.name,
+          to: demoWallet.address
         }
       ],
       selected: -1
     }
   },
+
   loaded: false,
-
-  loadDummyData: function() {
-    localStorage.setItem("disclaimerConfirm", "1");
-
-    PH.mainWallet = 0;
-    PH.wallets = dummyWallets;
-    PH.loaded = true;
-
-    PH.saveWallets();
-
-    location.reload();
-  },
 
   saveData: function() {
     localStorage.setItem('state', JSON.stringify(window.PH.state));
@@ -100,39 +50,6 @@ window.PH = {
     if (data) {
       window.PH.state = JSON.parse(data);
     }
-  },
-
-  saveWallets: function() {
-    localStorage.setItem("wallets", JSON.stringify(PH.wallets));
-
-    localStorage.setItem("main_wallet", Math.max(-1, Math.min(PH.wallets.length - 1, PH.mainWallet)));
-  },
-
-  addWallet: function(email, priv_key) {
-    // TODO: use NEO api
-    // and set current wallet to newly created wallet
-    return false;
-  },
-
-  selectWallet(number) {
-    if (number < 0 || number >= PH.wallets.length) {
-      console.warn('Illegal wallet selected');
-      return;
-    }
-
-    PH.mainWallet = number;
-
-    PH.saveWallets();
-
-    location.reload();
-  },
-
-  forgetWallet: function() {
-    if (PH.mainWallet === -1 || PH.mainWallet >= PH.wallets.length) { return; }
-
-    PH.wallets.splice(PH.mainWallet, 1);
-
-    PH.mainWallet = PH.wallets.length > 0 ? 0 : -1;
   },
 
   onLoad: function() {
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  root to: "landing#index"
+  root to: 'application#landing'
 
-  get 'inbox' => 'main#inbox'
-  get 'wallet' => 'main#wallet'
-  get 'files' => 'main#files'
-  get 'subs' => 'main#subs'
-  get 'disclaimer' => 'main#disclaimer'
-  get 'no_local_storage' => 'main#no_local_storage'
+  get 'app' => 'application#app'
 end
