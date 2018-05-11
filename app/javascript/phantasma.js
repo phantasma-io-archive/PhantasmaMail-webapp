77704caeb054a920e0952c546f7808957442fa90
Gonçalo Ferreira <gonmf@sapo.pt>
2018-05-11 21:58:49 +0100

    Multiple enhancemenets

--- app/javascript/phantasma.js
+++ app/javascript/phantasma.js
@@ -7,6 +7,25 @@ var demoSender = {
                    address: Math.random().toString(16).substring(2)
                  };
 
+var dummyContent = [
+  {
+    title: 'Abstract',
+    time: 1525623012,
+    body: "A purely peer-to-peer version of electronic cash would allow online payments to be sent directly from one party to another without going through a financial institution. Digital signatures provide part of the solution, but the main benefits are lost if a trusted third party is still required to prevent double-spending. We propose a solution to the double-spending problem using a peer-to-peer network. The network timestamps transactions by hashing them into an ongoing chain of hash-based proof-of-work, forming a record that cannot be changed without redoing the proof-of-work. The longest chain not only serves as proof of the sequence of events witnessed, but proof that it came from the largest pool of CPU power. As long as a majority of CPU power is controlled by nodes that are not cooperating to attack the network, they'll generate the longest chain and outpace attackers.<br/><br/>Cheers,<br/>PH Dev",
+    from: demoSender.address,
+    from_name: demoSender.name,
+    to: 'xxx'
+  },
+  {
+    title: 'Introduction',
+    time: 1525623012,
+    body: "Commerce on the Internet has come to rely almost exclusively on financial institutions serving as trusted third parties to process electronic payments. While the system works well enough for most transactions, it still suffers from the inherent weaknesses of the trust based model. Completely non-reversible transactions are not really possible, since financial institutions cannot avoid mediating disputes. The cost of mediation increases transaction costs, limiting the minimum practical transaction size and cutting off the possibility for small casual transactions, and there is a broader cost in the loss of ability to make non-reversible payments for nonreversible services. With the possibility of reversal, the need for trust spreads. Merchants must be wary of their customers, hassling them for more information than they would otherwise need. A certain percentage of fraud is accepted as unavoidable. These costs and payment uncertainties can be avoided in person by using physical currency, but no mechanism exists to make payments over a communications channel without a trusted party.",
+    from: demoSender.address,
+    from_name: demoSender.name,
+    to: 'xxx'
+  }
+  ];
+
 window.PH = {
   state: {
     mainWallet: -1,
@@ -14,24 +33,7 @@ window.PH = {
     section: 'inbox',
     topSection: 'mail',
     inbox: {
-      items: [
-        {
-          title: 'Abstract',
-          time: 1525623012,
-          body: "A purely peer-to-peer version of electronic cash would allow online payments to be sent directly from one party to another without going through a financial institution. Digital signatures provide part of the solution, but the main benefits are lost if a trusted third party is still required to prevent double-spending. We propose a solution to the double-spending problem using a peer-to-peer network. The network timestamps transactions by hashing them into an ongoing chain of hash-based proof-of-work, forming a record that cannot be changed without redoing the proof-of-work. The longest chain not only serves as proof of the sequence of events witnessed, but proof that it came from the largest pool of CPU power. As long as a majority of CPU power is controlled by nodes that are not cooperating to attack the network, they'll generate the longest chain and outpace attackers.<br/><br/>Cheers,<br/>PH Dev",
-          from: demoSender.address,
-          from_name: demoSender.name,
-          to: 'xxx'
-        },
-        {
-          title: 'Introduction',
-          time: 1525623012,
-          body: "Commerce on the Internet has come to rely almost exclusively on financial institutions serving as trusted third parties to process electronic payments. While the system works well enough for most transactions, it still suffers from the inherent weaknesses of the trust based model. Completely non-reversible transactions are not really possible, since financial institutions cannot avoid mediating disputes. The cost of mediation increases transaction costs, limiting the minimum practical transaction size and cutting off the possibility for small casual transactions, and there is a broader cost in the loss of ability to make non-reversible payments for nonreversible services. With the possibility of reversal, the need for trust spreads. Merchants must be wary of their customers, hassling them for more information than they would otherwise need. A certain percentage of fraud is accepted as unavoidable. These costs and payment uncertainties can be avoided in person by using physical currency, but no mechanism exists to make payments over a communications channel without a trusted party.",
-          from: demoSender.address,
-          from_name: demoSender.name,
-          to: 'xxx'
-        }
-      ],
+      items: [],
       selected: -1
     }
   },
@@ -116,7 +118,8 @@ window.PH = {
         console.log(res);
 
         if (callback) {
-          callback(res);
+          // TODO
+          callback();
         }
       });
     },
@@ -142,11 +145,50 @@ window.PH = {
       };
 
       Neon.doInvoke(config).then(res => {
-        console.log(res);
+        // TODO:
 
         if (callback) {
-          callback(res);
+          // TODO
+          callback(dummyContent.length);
         }
+      });
+    },
+
+    getMailContent: function(index, callback) {
+      // TODO:
+
+      if (callback) {
+        // TODO
+        return callback(dummyContent[index]);
+      }
+    },
+
+    sendMessage: function(recipient, message, callback) {
+      // TODO
+      if (callback) {
+        callback();
+      }
+    },
+
+    fetchInbox: function(callback) {
+      PH.contract.getMailCount(function(count) {
+        recursiveInboxFetch = function(acc1, index1, count1, callback1) {
+          if (count1 === index1) {
+            // Finished
+            PH.state.inbox.items = acc1;
+            PH.saveData();
+            if (callback1) {
+              callback1();
+            }
+            return;
+          }
+
+          PH.contract.getMailContent(index1, function(content) {
+            acc1.push(content);
+            recursiveInboxFetch(acc1, index1 + 1, count1, callback1);
+          });
+        };
+        recursiveInboxFetch([], 0, count, callback);
       });
     }
   }
